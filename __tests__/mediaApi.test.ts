e67f5a460c095e3c300508db95f2451c6788601f
akefdajani-sketch <akefdajani@gmail.com>
2026-03-25 01:12:16 +0300

    Delete __tests__ directory

--- __tests__/mediaApi.test.ts
+++
@@ -1,6 +0,0 @@
-describe('mediaApi', () => {
-  it('builds listMediaAssets query correctly', async () => {});
-  it('uploads media asset with form data', async () => {});
-  it('assigns media asset with correct payload', async () => {});
-  it('removes media assignment with delete payload', async () => {});
-});
